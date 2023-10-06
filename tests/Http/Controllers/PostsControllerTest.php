b1dc3d78cd039c1682fcb1b95229a06d16c0462d
butburg <fire.fm@hotmail.de>
2023-10-06 14:05:20 +0200

    installed and tested pest, a better syntax for phpunit basically

---
+++ tests/Http/Controllers/PostsControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Http\Controllers;
+
+use App\Http\Controllers\PostsController;
+use PHPUnit\Framework\TestCase;
+
+class PostsControllerTest extends TestCase
+{
+
+    public function testStore()
+    {
+
+    }
+
+    public function testUpdate()
+    {
+
+    }
+
+    public function testIndex()
+    {
+
+    }
+
+    public function testCreate()
+    {
+
+    }
+
+    public function testEdit()
+    {
+
+    }
+
+    public function testDestroy()
+    {
+
+    }
+
+    public function testShow()
+    {
+
+    }
+}
